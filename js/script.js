fb0268702ac5fec53009b20460b7c142b44fa488
Muriel MARCHAL <muriel.marchal@hotmail.com>
2023-03-07 11:30:28 +0100

    portfolio

---
+++ js/script.js
@@ -0,0 +1,28 @@
+//hide and see descriptions of the projects
+let projectBooki = document.querySelector(".project_booki");
+let projectOhmyfood = document.querySelector(".project_ohmyfood");
+let projectChouette = document.querySelector(".project_chouette");
+let projectKanap = document.querySelector(".project_kanap");
+let projectPiiquante = document.querySelector(".project_piiquante");
+let projectGroupomania = document.querySelector(".project_groupomania");
+
+function showHideBooki() {
+    projectBooki.classList.toggle("hide");
+}
+function showHideOhmyfood() {
+    projectOhmyfood.classList.toggle("hide");
+}
+function showHideChouette() {
+    projectChouette.classList.toggle("hide");
+}
+function showHideKanap() {
+    projectKanap.classList.toggle("hide");
+}
+function showHidePiiquante() {
+    projectPiiquante.classList.toggle("hide");
+}
+function showHideGroupomania() {
+    projectGroupomania.classList.toggle("hide");
+}
+
+//carousel projects
