5eb98ba3ef37316cdce9b8137a95784df991b9a3
Muriel MARCHAL <muriel.marchal@hotmail.com>
2023-03-22 18:36:34 +0100

    ajout html et modification js carousel

--- js/script.js
+++ js/script.js
@@ -1,26 +1,24 @@
-//hide and see descriptions of the projects
-let projectBooki = document.querySelector(".project_booki");
-let projectOhmyfood = document.querySelector(".project_ohmyfood");
-let projectChouette = document.querySelector(".project_chouette");
-let projectKanap = document.querySelector(".project_kanap");
-let projectPiiquante = document.querySelector(".project_piiquante");
-let projectGroupomania = document.querySelector(".project_groupomania");
-
-function showHideBooki() {
-    projectBooki.classList.toggle("hide");
-}
-function showHideOhmyfood() {
-    projectOhmyfood.classList.toggle("hide");
-}
-function showHideChouette() {
-    projectChouette.classList.toggle("hide");
-}
-function showHideKanap() {
-    projectKanap.classList.toggle("hide");
-}
-function showHidePiiquante() {
-    projectPiiquante.classList.toggle("hide");
-}
-function showHideGroupomania() {
-    projectGroupomania.classList.toggle("hide");
-}
+new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+  
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      520: {
+        slidesPerView: 2,
+      },
+      970: {
+        slidesPerView: 3,
+      }
+    }
+  });
