f7d53796c39a5d3c47497176a020e0f97ab4b4da
Muriel <muriel.marchal@hotmail.com>
2024-10-14 15:12:04 +0200

    add navbar fixed when scrolling

---
+++ js/fixed_navbar.js
@@ -0,0 +1,18 @@
+// Sélectionnez la barre de navigation
+const navbar = document.querySelector('.main-nav');
+
+// Obtenez la position de la navbar
+const navbarOffsetTop = navbar.offsetTop;
+
+// Fonction pour fixer la navbar lors du scroll
+function fixNavbar() {
+  if (window.scrollY >= navbarOffsetTop) {
+    navbar.classList.add('fixed-nav');
+  } else {
+    navbar.classList.remove('fixed-nav');
+    document.body.style.paddingTop = 0; // Supprime le padding lorsque la navbar n'est plus fixe
+  }
+}
+
+// Ajoutez un écouteur d'événement de scroll pour exécuter la fonction fixNavbar
+window.addEventListener('scroll', fixNavbar);
