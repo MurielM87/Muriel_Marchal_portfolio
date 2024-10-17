3989af8ac21b5e61584258df267c11cd60d99730
Muriel <muriel.marchal@hotmail.com>
2024-10-17 11:22:06 +0200

    modal others formations

--- js/modal.js
+++ js/modal.js
@@ -161,28 +161,51 @@ window.onclick = function(event) {
     modalSkilleos.style.display = "none";
   }
 }
-/*
-// Get the modal project 2 OCR
-let modal = document.getElementById("myModal");
+
+// Get the modal VueSchool
+let modalVueschool = document.getElementById("myModal_vueschool");
 
 // Get the button that opens the modal
-let btn = document.getElementById("btn");
+let btnVueschool = document.getElementById("btn_vueschool");
 
 // Get the <span> element that closes the modal
-let span = document.getElementsByClassName("close")[0];
+// let span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
+btnVueschool.onclick = function() {
+    modalVueschool.style.display = "block";
   }
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+  modalVueschool.style.display = "none";
 }
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == modalVueschool) {
+    modalVueschool.style.display = "none";
   }
 }
-*/
+// Get the modal autres formations
+let modalAutres = document.getElementById("myModal_autres");
+
+// Get the button that opens the modal
+let btnAutres = document.getElementById("btn_autres");
+
+// Get the <span> element that closes the modal
+// let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btnAutres.onclick = function() {
+    modalAutres.style.display = "block";
+  }
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modalAutres.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modalAutres) {
+    modalAutres.style.display = "none";
+  }
+}
+
